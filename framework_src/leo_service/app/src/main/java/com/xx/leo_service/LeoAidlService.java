beb6458f88c7eb98adde37e01b0989a11f69d274
leo <leoxianglei@126.com>
2020-07-12 10:53:24 +0800

    commit binder

---
+++ framework_src/leo_service/app/src/main/java/com/xx/leo_service/LeoAidlService.java
@@ -0,0 +1,45 @@
+package com.xx.leo_service;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.os.RemoteException;
+import android.support.annotation.Nullable;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LeoAidlService extends Service {
+
+    private ArrayList<Person> persons;
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        persons = new ArrayList<>();
+        Log.e("LeoAidlService", "success onBind");
+        return iBinder;
+    }
+
+    private IBinder iBinder = new ILeoAidl.Stub() {
+        @Override
+        public void addPerson(Person person) throws RemoteException {
+            persons.add(person);
+        }
+
+        @Override
+        public List<Person> getPersonList() throws RemoteException {
+            return persons;
+        }
+    };
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.e("LeoAidlService", "onCreate: success");
+    }
+}
+
+
+
